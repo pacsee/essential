40a2d09b608b592386aa5ad135b36c4d3d840c3a
Csaba Palankai <csaba.palankai@gmail.com>
2014-10-11 21:09:57 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-git clone git@github.com:pacsee/essential.git ~/essential
+# prerequisites
+# -------------
+# 
+# sudo apt-get install exuberant-ctags
+# sudo easy_install flake8
 
-cd ~/essential
 
 git pull
 git submodule init
@@ -10,8 +13,8 @@ git submodule sync
 git submodule update
 (cd .vim/bundle/vimproc; make)
 
-ln -bs ~/essential/bin ~/bin/
-ln -bs ~/essential/.vim ~/.vim/
+ln -s ~/essential/bin ~/bin
+ln -s ~/essential/.vim ~/.vim
 ln -bs ~/essential/.vimrc ~/.vimrc
 ln -bs ~/essential/.bashrc ~/.bashrc
 ln -bs ~/essential/.profile ~/.profile
@@ -21,5 +24,3 @@ ln -bs ~/essential/.git-config ~/.gitconfig
 ln -bs ~/essential/.ps1_vcs  ~/.ps1_vcs
 ln -bs ~/essential/.pythonstartup.py ~/.pythonstartup.py
 
-sudo apt-get install exuberant-ctags
-sudo easy_install flake8
