628aa938bd67e524bbf1c4eb38c7d0527537dae7
Csaba Palankai <csaba.palankai@gmail.com>
2016-12-29 14:23:05 +0000

    Add Emacs (vanilla)

---
+++ emacs.d/init.el
@@ -0,0 +1,26 @@
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(require 'evil)
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (neotree evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+; Custom configurations
+
+(load-file "~/.emacs.d/neotree-config.el")
---
+++ emacs.d/neotree-config.el
@@ -0,0 +1,10 @@
+(require 'neotree)
+(define-key evil-normal-state-map ",o" 'neotree-toggle)
+;(global-set-key ",o" 'neotree-toggle)
+
+(add-hook 'neotree-mode-hook
+            (lambda ()
+              (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+              (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+              (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
