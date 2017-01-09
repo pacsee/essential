5be282967ebe67e79b84476d60efaf5cf979bf41
Csaba Palankai <csaba.palankai@gmail.com>
2017-01-09 16:08:19 +0000

    Switch to use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,66 +1,112 @@
+;;;;; init.el - Emacs main configuration
+
+(setq lexical-binding t)
+(setq safe-local-variable-values (quote ((encoding . utf-8))))
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+
+;;; UI Setup
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
+
+;;; Packaging
+(setq package-enable-at-startup nil)
+;(setq custom-file (concat user-emacs-directory "customizations.el"))
+;(load custom-file t)
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
 
+;;; Ensure use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(setq use-package-verbose t)
+(setq use-package-always-ensure t)
+(require 'use-package)
 
+;;;; Packages to install
+;;; Main packages
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (dockerfile-mode ag ansible ansible-vault evil-magit ipython anaconda-mode python-mode evil-org org yaml-mode yasnippet magit sr-speedbar markdown-mode neotree evil)))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package evil
+    :config
+    (evil-mode 1)
+)
 
-; Custom configurations
-
-(defvar required-packages
-  '(
-    magit
-    yasnippet
-    yaml-mode
-    sr-speedbar
-    markdown-mode
-    neotree
-    evil
-  ) "a list of packages to ensure are installed at launch.")
-
-(load-file "~/.emacs.d/plugins/packages.el")
-
-(require 'evil)
-(evil-mode 1)
-
-(load-file "~/.emacs.d/neotree-config.el")
-(load-file "~/.emacs.d/relative-line-numbers.el")
-(load-file "~/.emacs.d/paren-mode.el")
-(load-file "~/.emacs.d/plugins/highlight-indentation.el")
-(load-file "~/.emacs.d/plugins/extra-scratches.el")
-
-(require 'sr-speedbar)
-(require 'yaml-mode)
-
-(load-file "~/.emacs.d/config/backup.el")
-(load-file "~/.emacs.d/keybindings.el")
-(setq initial-major-mode 'org-mode)
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
-(add-hook 'python-mode-hook 'anaconda-mode)
-(setq initial-scratch-message "\
+(use-package org
+    :config
+    (setq initial-major-mode 'org-mode)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((python . t)))
+    (setq initial-scratch-message "\
 # This buffer is for notes you don't want to save, and for Python code.
 # keys: ...
 ")
-(setq inhibit-startup-screen t)
-(setq python-python-command "~/anaconda/bin/python")
+)
+
+(use-package neotree
+    :config
+    (setq neo-window-width 35)
+    (define-key evil-normal-state-map ",o" 'neotree-toggle)
+    (add-hook 'neotree-mode-hook
+        (lambda ()
+            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter-vertical-split)
+            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+            (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-quick-look)))
+)
+
+(use-package magit)
+
+;;; Tools
+(use-package ag)
+(use-package yasnippet)
+
+;;; Modes
+(use-package yaml-mode)
+(use-package python-mode
+    :config
+    (setq python-python-command "~/anaconda/bin/python")
+)
+(use-package markdown-mode)
+(use-package dockerfile-mode)
+(use-package anaconda-mode
+    :config
+    (add-hook 'python-mode-hook 'anaconda-mode)
+)
+
+;;; Extensions
+(use-package ansible)
+(use-package ansible-vault)
+;(use-package ipython)
+
+(use-package sr-speedbar
+    :config
+    (define-key evil-normal-state-map ",t" 'sr-speedbar-toggle)
+)
+
+;;; Evil extensions
+(use-package evil-magit)
+(use-package evil-org)
+
+; Custom configurations / custom plugins
+
+
+(load-file "~/.emacs.d/relative-line-numbers.el")
+(load-file "~/.emacs.d/paren-mode.el")
+(load-file "~/.emacs.d/config/backup.el")
+(load-file "~/.emacs.d/keybindings.el")
+
+
+
+;(load-file "~/.emacs.d/plugins/highlight-indentation.el")
+;(load-file "~/.emacs.d/plugins/extra-scratches.el")
+
+;(require 'sr-speedbar)
+;(require 'yaml-mode)
+
+;(add-hook 'python-mode-hook 'anaconda-mode)
 
--- emacs.d/keybindings.el
+++ emacs.d/keybindings.el
@@ -1,13 +1,4 @@
-(define-key evil-normal-state-map ",t" 'sr-speedbar-toggle)
 (define-key evil-normal-state-map ",b" 'ibuffer)
 (define-key evil-normal-state-map ",w" 'toggle-truncate-lines)
-(define-key evil-normal-state-map ",o" 'neotree-toggle)
 
-(add-hook 'neotree-mode-hook
-            (lambda ()
-              (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter-vertical-split)
-              (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-              (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-quick-look)))
 
-(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
