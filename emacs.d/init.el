1252063ed211c41f97439738c3677ebcd23ac452
Csaba Palankai <csaba.palankai@gmail.com>
2017-01-13 13:44:37 +0000

    Switch to melpa stable

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -28,7 +28,10 @@
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ;;("melpa" . "https://melpa.org/packages/")
+))
+
 (package-initialize)
 
 ;;; Ensure use-package
