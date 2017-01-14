8a524b65b91bc22603035da56935c716040be7cc
Csaba Palankai <csaba.palankai@gmail.com>
2017-01-14 13:53:03 +0000

    Fix backup (check it later)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -216,7 +216,7 @@
 (setq
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
-    '(("." . backup-dir))    ; don't litter my fs tree
+    '(("." . "~/.emacs.d/backup/"))    ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
