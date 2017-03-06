6727d1a2441b917b0e4d321cbb3e1356e7f1e0f6
Csaba Palankai <csaba.palankai@gmail.com>
2017-03-06 09:26:19 +0000

    Exclude python cache files from neotree

--- emacs.d/config.el
+++ emacs.d/config.el
@@ -6,6 +6,9 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(neo-hidden-regexp-list
+   (quote
+    ("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "__pycache__$" "\\.egg-info$")))
  '(package-selected-packages
    (quote
     (zoom-frm evil-tabs general fill-column-indicator indent-guide grizzl flx-ido relative-line-numbers helm-projectile exec-path-from-shell evil-leader projectile-helm projectile use-package osx-clipboard theme-changer solarized-theme dockerfile-mode ag ansible ansible-vault evil-magit ipython anaconda-mode python-mode evil-org org yaml-mode yasnippet magit sr-speedbar markdown-mode neotree evil)))
