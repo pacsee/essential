02fececcef9565c577ca062271cd2fa912a160f4
Csaba Palankai <csaba.palankai@gmail.com>
2018-06-01 10:29:02 +0100

    More improvement necessary

--- bash/nodejs.bash
+++ bash/nodejs.bash
@@ -1,7 +1,7 @@
-if [ -z $NVM_LOADED ]; then
-    export NVM_LOADED=1
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-fi
+#if [ -z $NVM_LOADED ]; then
+export NVM_LOADED=1
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#fi
 
